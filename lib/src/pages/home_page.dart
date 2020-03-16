277d34c309835db5e72fbd769fc3059721e16e20
Fer <fer.ortego1988@gmail.com>
2020-03-16 13:30:45 +0100

    Peliculas provider

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
-
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -8,30 +8,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text('Peliculas en cines'),
-        backgroundColor: Colors.indigoAccent,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search), 
-            onPressed: (){})
-        ],
-      ),
-      body:Container(
-        child: Column(
-          children: <Widget>[
-            _swiperTarjetas(),
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Peliculas en cines'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {})
           ],
         ),
-      )
-     
-    );
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              _swiperTarjetas(),
+            ],
+          ),
+        ));
   }
 
   Widget _swiperTarjetas() {
-    return CardSwiper(
-      peliculas: [1,2,3,4,5]
-      );
+    final peliculasProvider = new PeliculasProvider();
+    peliculasProvider.getEncines();
+
+
+    return CardSwiper(peliculas: [1, 2, 3, 4, 5]);
   }
 }
